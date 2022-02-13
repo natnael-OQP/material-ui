076e4f570016107d9b240d1fd89024c4b5f6ebe5
papa-react <makiya112545@gmail.com>
2022-02-13 17:11:47 +0300

    custome theme created

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from "@material-ui/core";
+import { green } from "@material-ui/core/colors";
+
+export const theme = createTheme({
+    btn: {
+        background: green[200],
+    }
+ })
+
