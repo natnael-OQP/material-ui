f2f78062fd701fbc731f9c08de6793680a9158f2
papa-react <makiya112545@gmail.com>
2022-02-13 23:08:05 +0300

    style Leftbar

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import { Home } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+	container: {
+		height: "100vh",
+		background: theme.palette.primary.main,
+		color: "white",
+		paddingLeft: 10,
+		minWidth: 50,
+		paddingTop: theme.spacing(3),
+		[theme.breakpoints.up("sm")]: {
+			paddingTop: theme.spacing(3),
+			background: "white",
+			color: "#555",
+			border: "1px solid #e5e5e5",
+		},
+	},
+	item: {
+		display: "flex",
+		alignItems: "center",
+		marginBottom: theme.spacing(4),
+		[theme.breakpoints.up("sm")]: {
+			marginBottom: theme.spacing(3),
+			cursor: "pointer",
+		},
+	},
+	icon: {},
+	text: {
+		[theme.breakpoints.down("xs")]: {
+			display: "none",
+		},
+	},
+}));
 
 const LeftBar = () => {
-	return <div>LeftBar</div>;
+	const classes = useStyles();
+	return (
+		<Container className={classes.container}>
+			<div className={classes.item}>
+				<Home className={classes.icon} />
+				<Typography className={classes.text}>HomePage</Typography>
+			</div>
+			<div className={classes.item}>
+				<Home className={classes.icon} />
+				<Typography className={classes.text}>HomePage</Typography>
+			</div>
+			<div className={classes.item}>
+				<Home className={classes.icon} />
+				<Typography className={classes.text}>HomePage</Typography>
+			</div>
+			<div className={classes.item}>
+				<Home className={classes.icon} />
+				<Typography className={classes.text}>HomePage</Typography>
+			</div>
+			<div className={classes.item}>
+				<Home className={classes.icon} />
+				<Typography className={classes.text}>HomePage</Typography>
+			</div>
+			<div className={classes.item}>
+				<Home className={classes.icon} />
+				<Typography className={classes.text}>HomePage</Typography>
+			</div>
+			<div className={classes.item}>
+				<Home className={classes.icon} />
+				<Typography className={classes.text}>HomePage</Typography>
+			</div>
+			<div className={classes.item}>
+				<Home className={classes.icon} />
+				<Typography className={classes.text}>HomePage</Typography>
+			</div>
+		</Container>
+	);
 };
 
 export default LeftBar;
